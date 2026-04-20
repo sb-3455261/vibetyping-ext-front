a6352bb4f3ac5246518b72c29a470283084bfe84
sbbs <sbbs8668@gmail.com>
2026-04-20 20:33:06 +0000

    VBT-19 Registers VSMarketplace and OpenVSX

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+import * as vscode from "vscode"
+
+export function activate(context: vscode.ExtensionContext) {
+  const disposable = vscode.commands.registerCommand("vibetyping.helloWorld", () => {
+    vscode.window.showInformationMessage("Hello from Vibetyping!")
+  })
+  context.subscriptions.push(disposable)
+}
+
+export function deactivate() {}
